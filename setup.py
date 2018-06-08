52ee60c71579d79e9b0e157c19df958fd1508a7c
Benoit Hamelin <benoit@benoithamelin.com>
2018-06-08 15:29:25 -0400

    Remove auto version grab to fix package deployment
    The trick used herein to set the version from git tags is not only run
    from within this repo: it is also run on deployment sites, by pipenv
    and/or pip. Therefore, let's stick to fixed strings and update it by
    hand like peasants until we figure how to build setup.py from a
    template.

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 from subprocess import check_output
 
+    # version=check_output(("git", "describe", "--tags"), universal_newlines=True).strip(),
 setup(
     name='sim',
-    version=check_output(("git", "describe", "--tags"), universal_newlines=True).strip(),
+    version='1.0.1',
     packages=['sim'],
     install_requires=['greenlet==0.4.13'],
     description='Discrete Event Simulator',
