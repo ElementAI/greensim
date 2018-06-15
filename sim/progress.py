49cd19878c63506e85353e2b296153ff8a1cbc53
Benoit Hamelin <benoit@benoithamelin.com>
2018-06-15 13:17:28 -0400

    Add progress tracking system + unit tests

---
+++ sim/progress.py
@@ -0,0 +1,115 @@
+"""
+Progress tracking tools for simulations.
+"""
+
+from math import ceil, inf
+import sys
+import time
+from typing import cast, Callable, Sequence, Tuple, IO, Optional
+
+from sim import Simulator, Process
+
+
+MetricProgress = Sequence[float]
+MeasurerProgress = Callable[[], MetricProgress]  # Typically set up as a closure or a bound method.
+MeasureComparison = Sequence[Tuple[float, float]]
+CapturerProgress = Callable[[float, float, MeasureComparison], None]
+
+
+def combine(*measurers: Sequence[MeasurerProgress]) -> MetricProgress:
+    """Combines multiple progress measures into one metric."""
+    return sum((list(measurer()) for measurer in measurers), [])
+
+
+def sim_time(sim: Simulator):
+    """Progress measure based on the simulated clock."""
+    return lambda: [sim.now()]
+
+
+def capturer_print(file_dest_maybe: Optional[IO] = None):
+    file_dest: IO = file_dest_maybe or sys.stderr
+    def _print_progress(progress_min: float, rt_remaining: float, _mc: MeasureComparison) -> None:
+        percent_progress = progress_min * 100.0
+        time_remaining, unit = _display_time(rt_remaining)
+        print(
+            f"Progress: {percent_progress:.1f}% -- Time remaining: {time_remaining} {unit}          ",
+            end="\r",
+            file=self._output
+        )
+
+
+class ProgressTracker(Process):
+    """
+    Tracks progress against a certain end condition of the simulation (by
+    default, it is a certain duration on the simulated clock), reporting this
+    progress as the simulation chugs along. Stops the simulation once the
+    target has been reached.
+    """
+
+    def __init__(
+        self,
+        sim: Simulator,
+        measurer: MeasurerProgress,
+        target: MetricProgress,
+        interval_check: float,
+        capturer: Optional[CapturerProgress] = None
+    ):
+        super().__init__(sim)
+        self._measurer = measurer
+        self._target = target
+        self._interval_check = interval_check
+        self._capturer = capturer or capturer_print(sys.stderr)
+        self._rt_started: Optional[float] = None
+
+    def _measure(self) -> MeasureComparison:
+        return list(zip(self._measurer(), self._target))
+
+    def is_finished(self) -> bool:
+        return all(p >= t for p, t in self._measure())
+
+    def _run(self) -> None:
+        while True:
+            if self._rt_started is None:
+                self._rt_started = time.time()
+            else:
+                t = time.time()
+                rt_elapsed = t - cast(float, self._rt_started)
+                measure = self._measure()
+                ratio_progress_min = min(p / t for p, t in measure)
+                if ratio_progress_min == 0.0:
+                    rt_total_projected = inf
+                else:
+                    rt_total_projected = rt_elapsed / ratio_progress_min
+                self._capturer(ratio_progress_min, rt_total_projected - rt_elapsed, measure)
+
+                if self.is_finished():
+                    self.sim.stop()
+                    return
+
+            self.advance(self._interval_check)
+
+
+def _divide_round(dividend: int, divider: int) -> int:
+    return int(ceil(float(dividend) / divider))
+
+
+def _display_time(seconds: float) -> Tuple[int, str]:
+    delay = _divide_round(seconds, 1)
+    unit = "second"
+
+    if delay > 90:
+        delay = _divide_round(delay, 60)
+        unit = "minute"
+
+        if delay > 90:
+            delay = _divide_round(delay, 60)
+            unit = "hour"
+
+            if delay > 36:
+                delay = _divide_round(delay, 24)
+                unit = "day"
+
+    if delay > 1:
+        unit += "s"
+
+    return delay, unit
---
+++ tests/test_progress.py
@@ -0,0 +1,131 @@
+import time
+
+import pytest
+
+from sim import Simulator, Process
+from sim.progress import _display_time, _divide_round, ProgressTracker, sim_time, combine
+
+
+def test_divide_round():
+    assert _divide_round(60, 10) == 6
+    assert _divide_round(59, 10) == 6
+
+
+def test_display_seconds():
+    assert _display_time(0.8) == (1, "second")
+    assert _display_time(1.0) == (1, "second")
+    assert _display_time(90.0) == (90, "seconds")
+
+
+def test_display_minutes():
+    assert _display_time(90.1) == (2, "minutes")
+    assert _display_time(120.1) == (3, "minutes")
+    assert _display_time(90.0 * 60.0) == (90, "minutes")
+
+
+def test_display_hours():
+    assert _display_time(90.0 * 60.0 + 0.1) == (2, "hours")
+    assert _display_time(36.0 * 60.0 * 60.0) == (36, "hours")
+
+
+def test_display_days():
+    assert _display_time(36.0 * 60.0 * 60.0 + 0.1) == (2, "days")
+    assert _display_time(78 * 60.0 * 60.0) == (4, "days")
+
+
+def test_combine():
+    def _measure1():
+        return [5.6, 8.9]
+    def _measure2():
+        return [9.3]
+    def _measure3():
+        return [2.3, 9.2, 8.9]
+    assert combine(_measure1) == [5.6, 8.9]
+    assert combine(_measure1, _measure2, _measure3) == [5.6, 8.9, 9.3, 2.3, 9.2, 8.9]
+
+
+def has_tracker(sim, tracker):
+    return any(event == tracker._gr.switch for _, event in sim.events())
+
+
+def test_tracker_lifecycle():
+    def capture_pass(progress, rt_remaining, mc):
+        pass
+
+    sim = Simulator()
+    tracker = ProgressTracker(sim, sim_time(sim), [1000.0], 100.0, capture_pass)
+    assert has_tracker(sim, tracker)
+
+    def check_tracker(simm):
+        nonlocal tracker
+        assert has_tracker(simm, tracker)
+
+    sim.schedule(150.0, check_tracker)
+    sim.schedule(10000.0, lambda: sim.stop())
+    sim.start()
+    assert not has_tracker(sim, tracker)
+    assert sim.now() == pytest.approx(1000.0)
+
+
+def test_progress_capture():
+    log = []
+
+    def capture(progress_min, _rt_remaining, mc):
+        nonlocal log
+        log.append(progress_min)
+
+    a = 0
+    b = 0
+
+    def set_ab(new_a, new_b):
+        nonlocal a, b
+        def _event(sim):
+            nonlocal a, b
+            a = new_a
+            b = new_b
+        return _event
+
+    def measure():
+        nonlocal a, b
+        return (a, b)
+
+    sim = Simulator()
+    ProgressTracker(sim, measure, [10,10], 10.0, capture)
+    sim.schedule(15.0, set_ab(2, 0))
+    sim.schedule(25.0, set_ab(4, 1))
+    sim.schedule(35.0, set_ab(4, 6))
+    sim.schedule(45.0, set_ab(5, 9))
+    sim.schedule(55.0, set_ab(10, 10))
+    sim.schedule(100.0, lambda sim: sim.stop())
+    sim.start()
+
+    assert sim.now() == pytest.approx(60.0)
+    assert log == pytest.approx([0.0, 0.0, 0.1, 0.4, 0.5, 1.0])
+
+
+class Sleeper(Process):
+
+    def __init__(self, sim, interval, rt_delay):
+        super().__init__(sim)
+        self._interval = interval
+        self._rt_delay = rt_delay
+
+    def _run(self):
+        while True:
+            time.sleep(self._rt_delay)
+            self.advance(self._interval)
+
+
+def test_progress_real_time():
+    log = []
+
+    def capture(_progress_min, rt_remaining, mc):
+        nonlocal log
+        log.append(rt_remaining)
+
+    sim = Simulator()
+    ProgressTracker(sim, sim_time(sim), [100.0], 20.0, capture)
+    Sleeper(sim, 10.0, 0.1)
+    sim.start()
+
+    assert log == pytest.approx([0.8, 0.6, 0.4, 0.2, 0.0], abs=1e-2)
