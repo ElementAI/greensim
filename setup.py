df83ee04aacc9e6384e1dda962249ed09c3ae3c2
Benoit Hamelin <benoit@benoithamelin.com>
2018-06-15 13:36:54 -0400

    Bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from subprocess import check_output
     # version=check_output(("git", "describe", "--tags"), universal_newlines=True).strip(),
 setup(
     name='sim',
-    version='1.1.0',
+    version='1.1.1',
     packages=['sim'],
     install_requires=['greenlet==0.4.13'],
     description='Discrete Event Simulator',
